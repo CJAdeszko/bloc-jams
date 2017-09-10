54a9f5b30194e422df11b0c0d475aba7d22a2d79
Christopher Adeszko <cjadeszko@gmail.com>
2017-09-09 17:54:14 -0700

    Assignment 13: completed

--- scripts/album.js
+++ scripts/album.js
@@ -65,7 +65,13 @@ var createSongRow = function(songNumber, songName, songLength) {
 };
 
 var findParentByClassName = function(element, targetClass) {
-    if (element) {
+  if(element.parentElement == undefined){
+    console.log("No parent found");
+  }
+  else if(element.parentElement.className !== targetClass){
+    console.log("No parent found with that class name");
+  }
+  else if(element) {
         var currentParent = element.parentElement;
         while (currentParent.className !== targetClass && currentParent.className !== null) {
             currentParent = currentParent.parentElement;
